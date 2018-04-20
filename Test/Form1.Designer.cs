5b2abeb35f95df7fe7042c90fea346f7aa971863
jingjiajie <932166095@qq.com>
2018-04-20 15:06:29 +0800

    added images for wiki

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -36,25 +36,27 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.basicView1 = new FrontWork.BasicView();
             this.configuration = new FrontWork.Configuration();
             this.model1 = new FrontWork.Model();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pagerSearchJsonRESTAdapter1 = new FrontWork.PagerSearchJsonRESTAdapter();
             this.searchWidget1 = new FrontWork.SearchView();
             this.jsonRESTSynchronizer1 = new FrontWork.JsonRESTSynchronizer();
             this.reoGridView1 = new FrontWork.ReoGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.pagerView1 = new FrontWork.PagerView();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("黑体", 10F);
-            this.button2.Location = new System.Drawing.Point(18, 125);
+            this.button2.Location = new System.Drawing.Point(16, 92);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(160, 69);
             this.button2.TabIndex = 3;
@@ -65,7 +67,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("黑体", 10F);
-            this.button3.Location = new System.Drawing.Point(18, 44);
+            this.button3.Location = new System.Drawing.Point(16, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(160, 68);
             this.button3.TabIndex = 4;
@@ -76,7 +78,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("黑体", 10F);
-            this.button4.Location = new System.Drawing.Point(18, 200);
+            this.button4.Location = new System.Drawing.Point(16, 174);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(160, 68);
             this.button4.TabIndex = 5;
@@ -89,60 +91,33 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.tableLayoutPanel1.Controls.Add(this.basicView1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.searchWidget1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.pagerView1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1509, 793);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1509, 812);
             this.tableLayoutPanel1.TabIndex = 16;
-            // 
-            // panel1
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
-            this.panel1.Controls.Add(this.pagerSearchJsonRESTAdapter1);
-            this.panel1.Controls.Add(this.configuration);
-            this.panel1.Controls.Add(this.jsonRESTSynchronizer1);
-            this.panel1.Controls.Add(this.model1);
-            this.panel1.Controls.Add(this.reoGridView1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 329);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1509, 403);
-            this.panel1.TabIndex = 17;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(1312, 3);
-            this.panel2.Name = "panel2";
-            this.tableLayoutPanel1.SetRowSpan(this.panel2, 2);
-            this.panel2.Size = new System.Drawing.Size(194, 323);
-            this.panel2.TabIndex = 18;
             // 
             // basicView1
             // 
             this.basicView1.Configuration = this.configuration;
             this.basicView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.basicView1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.basicView1.Location = new System.Drawing.Point(0, 60);
+            this.basicView1.Location = new System.Drawing.Point(0, 0);
             this.basicView1.Margin = new System.Windows.Forms.Padding(0);
             this.basicView1.Model = this.model1;
             this.basicView1.Name = "basicView1";
-            this.basicView1.Size = new System.Drawing.Size(1309, 269);
+            this.basicView1.Padding = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.basicView1.Size = new System.Drawing.Size(1309, 201);
             this.basicView1.TabIndex = 13;
             // 
             // configuration
@@ -150,7 +125,7 @@
             this.configuration.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("configuration.BackgroundImage")));
             this.configuration.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.configuration.ConfigurationString = resources.GetString("configuration.ConfigurationString");
-            this.configuration.Location = new System.Drawing.Point(945, 187);
+            this.configuration.Location = new System.Drawing.Point(1243, 202);
             this.configuration.Mode = "default";
             this.configuration.Name = "configuration";
             this.configuration.Size = new System.Drawing.Size(180, 180);
@@ -162,12 +137,27 @@
             this.model1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.model1.Configuration = this.configuration;
             this.model1.FirstSelectionRange = null;
-            this.model1.Location = new System.Drawing.Point(768, 187);
+            this.model1.Location = new System.Drawing.Point(901, 29);
             this.model1.Name = "model1";
             this.model1.SelectionRange = new FrontWork.Range[0];
             this.model1.Size = new System.Drawing.Size(180, 180);
             this.model1.TabIndex = 11;
             this.model1.Load += new System.EventHandler(this.model1_Load);
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.pagerSearchJsonRESTAdapter1);
+            this.panel1.Controls.Add(this.configuration);
+            this.panel1.Controls.Add(this.jsonRESTSynchronizer1);
+            this.panel1.Controls.Add(this.model1);
+            this.panel1.Controls.Add(this.reoGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 281);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1509, 470);
+            this.panel1.TabIndex = 17;
             // 
             // pagerSearchJsonRESTAdapter1
             // 
@@ -194,7 +184,7 @@
             this.searchWidget1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.searchWidget1.Location = new System.Drawing.Point(3, 3);
             this.searchWidget1.Name = "searchWidget1";
-            this.searchWidget1.Size = new System.Drawing.Size(1303, 54);
+            this.searchWidget1.Size = new System.Drawing.Size(1303, 74);
             this.searchWidget1.TabIndex = 6;
             // 
             // jsonRESTSynchronizer1
@@ -215,24 +205,47 @@
             this.reoGridView1.Margin = new System.Windows.Forms.Padding(0);
             this.reoGridView1.Model = this.model1;
             this.reoGridView1.Name = "reoGridView1";
-            this.reoGridView1.Size = new System.Drawing.Size(1509, 403);
+            this.reoGridView1.Size = new System.Drawing.Size(1509, 470);
             this.reoGridView1.TabIndex = 9;
             this.reoGridView1.Load += new System.EventHandler(this.reoGridView1_Load);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(1312, 3);
+            this.panel2.Name = "panel2";
+            this.tableLayoutPanel1.SetRowSpan(this.panel2, 2);
+            this.panel2.Size = new System.Drawing.Size(194, 275);
+            this.panel2.TabIndex = 18;
             // 
             // pagerView1
             // 
             this.pagerView1.Configuration = null;
-            this.pagerView1.Location = new System.Drawing.Point(3, 735);
+            this.pagerView1.Location = new System.Drawing.Point(3, 754);
             this.pagerView1.Name = "pagerView1";
+            this.pagerView1.PageSize = ((long)(50));
             this.pagerView1.Size = new System.Drawing.Size(1280, 55);
             this.pagerView1.TabIndex = 19;
             this.pagerView1.Load += new System.EventHandler(this.pagerView1_Load);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.basicView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 80);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1309, 201);
+            this.panel3.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1509, 793);
+            this.ClientSize = new System.Drawing.Size(1509, 812);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "FrontWork框架测试";
@@ -241,6 +254,7 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -260,6 +274,7 @@
         private FrontWork.PagerView pagerView1;
         private FrontWork.SearchView searchWidget1;
         private FrontWork.PagerSearchJsonRESTAdapter pagerSearchJsonRESTAdapter1;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
